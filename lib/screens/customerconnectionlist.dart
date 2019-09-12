739ae105737d3cd29b9b05f02871cdd14b193011
Pants <chancefinley0628@gmail.com>
2019-09-12 09:54:05 -0400

    Connection List Filter Screen Update
    Dialogs now hold state and can be updated. Saving now before i push the new findings to the Ticket Dashboard and the Customer Screen

--- lib/screens/customerconnectionlist.dart
+++ lib/screens/customerconnectionlist.dart
@@ -7,6 +7,8 @@ import 'package:st_two/screens/connection.dart';
 
 enum ConfirmAction { CANCEL, ACCEPT }
 
+String versionfilter;
+
 class CustomerConnectionList extends StatefulWidget {
   CustomerConnectionList({Key key, this.title}) : super(key: key);
 
@@ -17,8 +19,7 @@ class CustomerConnectionList extends StatefulWidget {
 }
 
 class _CustomerConnectionListState extends State<CustomerConnectionList> {
-  String _versionSelection = 'All Versions';
-  List<DropdownMenuItem<String>> versiondropdown = [];
+  StreamController<String> mycontroller = StreamController();
 
   TextEditingController tecSearch = TextEditingController();
   var searchFocusNode = FocusNode();
@@ -29,7 +30,6 @@ class _CustomerConnectionListState extends State<CustomerConnectionList> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    loadFilterDropdowns();
     tecSearch.addListener(() {
       setState(() {
         filter = tecSearch.text;
@@ -42,6 +42,7 @@ class _CustomerConnectionListState extends State<CustomerConnectionList> {
     // TODO: implement dispose
     tecSearch.dispose();
     searchFocusNode.dispose();
+    versionfilter = null;
     super.dispose();
   }
 
@@ -67,84 +68,100 @@ class _CustomerConnectionListState extends State<CustomerConnectionList> {
                 child: GestureDetector(
                   child: Icon(Icons.filter_list),
                   onTap: () {
-                    _asyncConfirmDialog(context);
+                    _asyncConfirmDialog(context).then((_)=>setState((){}));
                   },
                 )),
           )
         ],
       ),
-      body: FutureBuilder(
-        future: loadConnectionList(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              print(snapshot.error);
-            }
-            return snapshot.hasData
-                ? ListView.builder(
-                    itemCount: snapshot.data.ccon.length,
-                    itemBuilder: (context, index) {
-                      return filter == null || filter == ""
-                          ? GestureDetector(
-                              onTap: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ConnectionPage(
-                                    title: 'View Connection',
-                                    cc: snapshot.data.ccon[index],
-                                    readonly: true,
-                                  ),
-                                ),
-                              ),
-                              child: Card(
-                                elevation: 5,
-                                child: ListTile(
-                                  title: Text(snapshot
-                                      .data.ccon[index].customername
-                                      .toString()),
-                                  subtitle: Text('Version: ' +
-                                      snapshot.data.ccon[index].erpversion
-                                          .toString(),),
-                                  trailing: Text(snapshot.data.ccon[index].customerinactive.toString(),),
-                                ),
-                              ))
-                          : snapshot.data.ccon[index].customername
-                                      .contains(filter) ||
+      body: Stack(
+        children: <Widget>[
+          FutureBuilder(
+            future: loadConnectionList(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  print(snapshot.error);
+                }
+                return snapshot.hasData
+                    ? ListView.builder(
+                  itemCount: snapshot.data.ccon.length,
+                  itemBuilder: (context, index) {
+                    return filter == null || filter == ""
+                        ? GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ConnectionPage(
+                              title: 'View Connection',
+                              cc: snapshot.data.ccon[index],
+                              readonly: true,
+                            ),
+                          ),
+                        ),
+                        child: Card(
+                          elevation: 5,
+                          child: ListTile(
+                            title: Text(snapshot
+                                .data.ccon[index].customername
+                                .toString()),
+                            subtitle: Text(
+                              'Version: ' +
                                   snapshot.data.ccon[index].erpversion
-                                      .toString()
-                                      .contains(filter)
-                              ? GestureDetector(
-                                  onTap: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => ConnectionPage(
-                                        title: 'View Connection',
-                                        cc: snapshot.data.ccon[index],
-                                        readonly: true,
-                                      ),
-                                    ),
-                                  ),
-                                  child: Card(
-                                    elevation: 5,
-                                    child: ListTile(
-                                      title: Text(snapshot
-                                          .data.ccon[index].customername
-                                          .toString()),
-                                      subtitle: Text('Version: ' +
-                                          snapshot.data.ccon[index].erpversion
-                                              .toString(),),
-                                      trailing: Text(snapshot.data.ccon[index].customerinactive.toString(),),
-                                    ),
-                                  ),
-                                )
-                              : Container();
-                    },
-                  )
-                : Center(child: CircularProgressIndicator());
-          } else {
-            return CircularProgressIndicator();
-          }
-        },
+                                      .toString(),
+                            ),
+                            trailing: Text(
+                              snapshot.data.ccon[index].customerinactive
+                                  .toString(),
+                            ),
+                          ),
+                        ))
+                        : snapshot.data.ccon[index].customername
+                        .contains(filter) ||
+                        snapshot.data.ccon[index].erpversion
+                            .toString()
+                            .contains(filter)
+                        ? GestureDetector(
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ConnectionPage(
+                            title: 'View Connection',
+                            cc: snapshot.data.ccon[index],
+                            readonly: true,
+                          ),
+                        ),
+                      ),
+                      child: Card(
+                        elevation: 5,
+                        child: ListTile(
+                          title: Text(snapshot
+                              .data.ccon[index].customername
+                              .toString()),
+                          subtitle: Text(
+                            'Version: ' +
+                                snapshot.data.ccon[index].erpversion
+                                    .toString(),
+                          ),
+                          trailing: Text(
+                            snapshot
+                                .data.ccon[index].customerinactive
+                                .toString(),
+                          ),
+                        ),
+                      ),
+                    )
+                        : Container();
+                  },
+                )
+                    : Center(child: CircularProgressIndicator());
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+          isListFiltered(),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Theme.of(context).primaryColor,
@@ -190,61 +207,26 @@ class _CustomerConnectionListState extends State<CustomerConnectionList> {
     setState(() {});
   }
 
+  Widget isListFiltered() {
+    if(versionfilter != null){
+      return Center(
+        child: Text('Filter has been set'),
+      );
+    } else {
+      return Center(
+        child: Container(),
+      );
+    }
+  }
+
   Future<ConfirmAction> _asyncConfirmDialog(BuildContext context) async {
     return showDialog<ConfirmAction>(
       context: context,
       barrierDismissible: false, // user must tap button for close dialog!
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Filters'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              DropdownButton<String>(
-                isExpanded: true,
-                value: _versionSelection,
-                onChanged: (String newValue) {
-                  setState(() {
-                    _versionSelection = newValue;
-                  });
-
-                  print(_versionSelection);
-                },
-                items: versiondropdown,
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: const Text('CANCEL'),
-              onPressed: () {
-                Navigator.of(context).pop(ConfirmAction.CANCEL);
-              },
-            ),
-            FlatButton(
-              child: const Text('ACCEPT'),
-              onPressed: () {
-                Navigator.of(context).pop(ConfirmAction.ACCEPT);
-              },
-            )
-          ],
-        );
-      },
+        return MyDialog();
+      }
     );
-  }
-
-  void loadFilterDropdowns() async {
-    String jsonString = await DefaultAssetBundle.of(context)
-        .loadString("assets/versiondropdowndata.json");
-    final jsonResponse = json.decode(jsonString);
-    VersionListdd versionlist = new VersionListdd.fromJson(jsonResponse);
-
-    for (var i = 0; i < versionlist.versions.length; i++) {
-      versiondropdown.add(DropdownMenuItem(
-        value: versionlist.versions[i].id,
-        child: Text(versionlist.versions[i].id.toString()),
-      ));
-    }
   }
 
   Future<ConnectionList> loadConnectionList() async {
@@ -253,6 +235,101 @@ class _CustomerConnectionListState extends State<CustomerConnectionList> {
     final jsonResponse = json.decode(jsonString);
     ConnectionList connectionlist = new ConnectionList.fromJson(jsonResponse);
     print('Connection list loaded for Customer Connection List Screen');
+
+    if(versionfilter != null){
+      connectionlist.ccon.removeWhere((item) => item.erpversion != versionfilter);
+    }
     return connectionlist;
+  }
+}
+
+class MyDialog extends StatefulWidget {
+  @override
+  _MyDialogState createState() => new _MyDialogState();
+}
+
+class _MyDialogState extends State<MyDialog> {
+
+  String _versionSelection = 'All Versions';
+  List<DropdownMenuItem<String>> versiondropdown = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<List<DropdownMenuItem<String>>> loadFilterDropdowns() async {
+
+    if(versiondropdown.length < 1){
+      String jsonString = await DefaultAssetBundle.of(context)
+          .loadString("assets/versiondropdowndata.json");
+      final jsonResponse = json.decode(jsonString);
+      VersionListdd versionlist = new VersionListdd.fromJson(jsonResponse);
+
+      for (var i = 0; i < versionlist.versions.length; i++) {
+        versiondropdown.add(DropdownMenuItem(
+          value: versionlist.versions[i].id.toString(),
+          child: Text(versionlist.versions[i].id.toString()),
+        ));
+      }
+    }
+
+    return versiondropdown;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<DropdownMenuItem<String>>>(
+      future: loadFilterDropdowns(),
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.done){
+          if(snapshot.hasError){
+            print(snapshot.error.toString());
+          }
+          return snapshot.hasData ?
+          AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                DropdownButton<String>(
+                  isExpanded: true,
+                  value: _versionSelection,
+                  onChanged: (String newValue) {
+                    setState((){
+                      print(newValue.toString());
+                      _versionSelection = newValue;
+
+                    });
+                  },
+                  items: snapshot.data,
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop(ConfirmAction.CANCEL);
+                },
+              ),
+              FlatButton(
+                child: const Text('ACCEPT'),
+                onPressed: () {
+                  Navigator.of(context).pop(ConfirmAction.ACCEPT);
+                  versionfilter = _versionSelection;
+                  setState((){});
+                  print('Filter Accepted: '+_versionSelection);
+                },
+              )
+            ],
+          )
+              :
+        Center(child: Container());
+        } else {
+          return Center(child:Container());
+        }
+      },
+    );
   }
 }
